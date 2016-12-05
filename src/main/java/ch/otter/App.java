800e40b3f98cdce4cdaa7279e9490f4532755abc
Felice Serena <felice@serena-mueller.ch>
2016-12-05 10:00:48 +0100

    mvn repo init

---
+++ src/main/java/ch/otter/App.java
@@ -0,0 +1,13 @@
+package ch.otter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

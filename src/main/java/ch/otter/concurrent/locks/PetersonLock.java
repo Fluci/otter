bbb94df27008e9914ef9dae388babca1daa0faa1
Felice Serena <felice@serena-mueller.ch>
2016-12-07 10:40:29 +0100

    PetersonLock: fixed id distribution

--- src/main/java/ch/otter/concurrent/locks/PetersonLock.java
+++ src/main/java/ch/otter/concurrent/locks/PetersonLock.java
@@ -18,7 +18,7 @@ import java.util.concurrent.locks.Lock;
  */
 public class PetersonLock extends AbstractLock {
     private final AtomicBoolean[] ids;
-    private final ThreadLocal<T>
+    private final ThreadLocal<Integer> id = ThreadLocal.withInitial(() -> new Integer(-1));
     private final AtomicIntegerArray level;
     private final AtomicIntegerArray last_to_enter;
     private final int threadCount;
@@ -32,17 +32,20 @@ public class PetersonLock extends AbstractLock {
         this.threadCount = threadCount;
         level = new AtomicIntegerArray(threadCount);
         last_to_enter = new AtomicIntegerArray(threadCount-1);
+        ids = new AtomicBoolean[threadCount];
         for(int i = 0; i < threadCount; i+= 1) {
             level.set(i, -1);
+            ids[i] = new AtomicBoolean(false);
         }
     }
 
     public void lock() {
-        int id = getId();
+        int myId = getId();
+        id.set(myId);
         for(int l = 0; l < threadCount-1; l += 1) {
-            level.set(id, l);
-            last_to_enter.set(l, id);
-            while (last_to_enter.get(l) == id && existsKGreaterL(l, id));
+            level.set(myId, l);
+            last_to_enter.set(l, myId);
+            while (last_to_enter.get(l) == myId && existsKGreaterL(l, myId));
         }
     }
 
@@ -65,12 +68,14 @@ public class PetersonLock extends AbstractLock {
     }
 
     public boolean tryLock() {
-        int id = getId();
+        int myId = getId();
+        id.set(myId);
         for(int l = 0; l < threadCount-1; l += 1) {
-            level.set(id, l);
-            last_to_enter.set(l, id);
-            if (last_to_enter.get(l) == id && existsKGreaterL(l, id)) {
-                level.set(id, -1);
+            level.set(myId, l);
+            last_to_enter.set(l, myId);
+            if (last_to_enter.get(l) == myId && existsKGreaterL(l, myId)) {
+                level.set(myId, -1);
+                releaseId(myId);
                 return false;
             }
         }
@@ -82,7 +87,9 @@ public class PetersonLock extends AbstractLock {
     }
 
     public void unlock() {
-        level.set(getId(), -1);
+        int myId = id.get();
+        level.set(myId, -1);
+        releaseId(myId);
     }
 
     public Condition newCondition() {
@@ -90,6 +97,23 @@ public class PetersonLock extends AbstractLock {
     }
 
     private int getId() {
-        return (int)Thread.currentThread().getId()%threadCount;
+        int myId = id.get();
+        if(myId != -1) {
+            // first try to acquire old id
+            if(!ids[myId].getAndSet(true)){
+                return myId;
+            }
+        }
+        while(true) {
+            for(int i = 0; i < threadCount; i += 1) {
+                if(!ids[i].getAndSet(true)){
+                    return i;
+                }
+            }
+        }
+    }
+
+    private void releaseId(int anId) {
+        ids[anId].set(false);
     }
 }
